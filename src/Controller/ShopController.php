066e9dc31c462bb9af15b84182ddb1d57a3049ce
Bryan <midekorbryan@gmail.com>
2023-04-27 16:51:02 +0200

    update git ignore

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -6,13 +6,15 @@ use App\Entity\Produit;
 use App\Form\ProduitType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/shop', name: 'shop')]
+#[Route('/shop', name: 'shop'),
+    IsGranted('ROLE_CLIENT')]
 class ShopController extends AbstractController
 {
     #[Route('/list', name: '_list')]
